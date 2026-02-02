ec5117ab94e39c6bb800e9a4a334c9346f786b06
Augustin Barillec <augustin-barillec@gmail.com>
2026-02-02 22:31:19 +0100

    style

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/game.css";
+import "./styles/panels.css";
+import "./styles/modals.css";
+import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
